e3bab3e3081e29f37dca9d8feabb6303c497558c
Christopher Luvumbo <christopherluvumbo@gmail.com>
2022-04-07 17:06:29 +0200

    add useState and useEffect

--- src/index.jsx
+++ src/index.jsx
@@ -11,8 +11,11 @@ import { createGlobalStyle } from 'styled-components'
 
 
 const GlobalStyle = createGlobalStyle`
-    div {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+      margin: 0;
     }
 `
 
